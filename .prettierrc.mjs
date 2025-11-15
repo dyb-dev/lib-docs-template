53fb8412f59821a5433a2ddf01932929e177ba91
dyb-dev <2737993954@qq.com>
2025-11-15 14:13:36 +0800

    🌈 style: 更新文件注释格式

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,10 +1,10 @@
 /*
  * @Author: dyb-dev
  * @Date: 2024-02-27 21:21:41
- * @LastEditors: dyb-dev
- * @LastEditTime: 2024-08-30 17:14:12
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2025-11-15 13:18:29
  * @FilePath: /lib-docs-template/.prettierrc.mjs
- * @Description: prettier配置文件 注意：每次配置文件的更改，建议重启一下vscode，否则可能不会生效
+ * @Description: prettier 配置 注意：每次配置的更改，建议重启一下编辑器，否则可能不会生效
  */
 
 import prettierConfig from "@dyb-dev/prettier-config"
--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,10 +1,10 @@
 /*
  * @Author: dyb-dev
  * @Date: 2024-03-20 16:17:30
- * @LastEditors: dyb-dev
- * @LastEditTime: 2024-08-30 17:14:24
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2025-11-15 13:18:37
  * @FilePath: /lib-docs-template/.stylelintrc.mjs
- * @Description: stylelint配置文件 注意：每次配置文件的更改，建议重启一下vscode，否则可能不会生效
+ * @Description: stylelint 配置 注意：每次配置的更改，建议重启一下编辑器，否则可能不会生效
  */
 
 export default {
